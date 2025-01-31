ac05dee73512d6f0c516ed5f9d77f8e2b6af8204
IFM <152510445+gitIliyaM@users.noreply.github.com>
2025-01-31 09:58:00 +0300

    Update Aspects.java

--- src/main/java/ru/t1/java/demo/aop/Aspects.java
+++ src/main/java/ru/t1/java/demo/aop/Aspects.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import java.util.stream.Collectors;
-import java.util.Arrays;
+import java.util.*;
 import org.slf4j.*;
 
 @Aspect
